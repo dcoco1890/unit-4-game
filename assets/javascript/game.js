b1d4208892766f005e0a8c84983d490176508373
herman2 <herman2@herman2s-MBP.fios-router.home>
2019-06-06 23:15:30 -0700

    getting content on page, started a little JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wins = 0;
+var losses = 0;
+var crystalFirst = {
+   value: function(){
+
+   }
+}
+
+function getValue(){
+    var x = Math.floor(Math.random() * 120) + 19;
+    console.log(x);
+}
+getValue();
